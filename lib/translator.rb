9cbbe2220d5f71c7716b7af3acc7e78eb668b48e
Kevin Sheehan <kshhn12@gmail.com>
2020-08-17 16:53:12 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -24,6 +24,7 @@ def get_japanese_emoticon(path, emoticon)
       result = meaning[:japanese]
     end
   end
+  binding.pry
   if result == nil
     result = "Sorry, that emoticon was not found" 
   end
